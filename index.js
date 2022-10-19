81a43ba6cb5585e0e18ee05605d1a4ec7be4fc87
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-10-19 17:42:39 +0700

    initial setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+console.log(PORT);
+
+// const { sequelize } = require("./lib/sequelize");
+
+// sequelize.sync({ alter: true });
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("<h1>Management Referral API</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log("Listening in port", PORT);
+});
